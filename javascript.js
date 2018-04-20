f2df36776226594d295975270a5b441031d2edfb
MichaelSParkin3 <MichaelSParkin3@gmail.com>
2018-04-19 17:39:15 -0700

    Added visible plugin and triggers anims when elements are scroll into viewport.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
+  //enabling niceScroll plugin
     $("#thisdiv").niceScroll({cursorcolor:"#00F"});
   $("body").niceScroll();
 
-  $(".blocker").inViewport(function(px){
-      if(px) $(this).addClass("slideboxinanim") ;
+  console.log("js is on");
+
+//triggers when window is scrolled
+  $(window).bind('scroll', function(){
+    console.log("scroll!");
+    //if any part of the skills block is visible then start entrance anim
+    if ($('.blocker').visible(true)) {
+      console.log("blocker");
+      $( ".blocker" ).addClass( "slideboxinanim" );
+    }
+
   });
 
 });
